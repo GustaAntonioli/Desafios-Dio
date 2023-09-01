6f29c692c09a9387c0bbee9abbaa8740f45616e9
GUSTAVO ANTONIOLI MEDEIROS <94537799+GustaAntonioli@users.noreply.github.com>
2023-09-01 19:30:26 -0300

    -

--- src/DesafioControleFluxo/Contador.java
+++ src/DesafioControleFluxo/Contador.java
@@ -1,6 +1,38 @@
 package DesafioControleFluxo;
+
+import java.util.Scanner;
+
 public class Contador {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		
+		try {
+			// Lendo o primeiro parâmetro
+			System.out.println("Informe o primeiro parâmetro: ");
+			int parametroUm = Integer.parseInt(terminal.nextLine());
+			
+			// Lendo o segundo parâmetro
+			System.out.println("Informe o segundo parâmetro: ");
+			int parametroDois = Integer.parseInt(terminal.nextLine());
+			
+			// Validando se parametroDois é maior ou igual que parametroUm
+			if (parametroDois <= parametroUm){
+				throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior ou igual ao primeiro");
+			}
+			
+			// Imprimindo os números
+			for (int i = parametroUm; i <= parametroDois; i++) {
+				System.out.println("Imprimindo o número " + i);
+			}
+		
+		}catch (ParametrosInvalidosException exception) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior ou igual ao primeiro
+            System.out.println(exception.getMessage());
+        }
+		
+		// fechando o scanner
+		//terminal.close();
+		
+	}
 }
--- src/DesafioControleFluxo/ParametrosInvalidosException.java
+++ src/DesafioControleFluxo/ParametrosInvalidosException.java
@@ -1,4 +1,13 @@
 package DesafioControleFluxo;
-public class ParametrosInvalidosException {
+
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String string) {
+        super(string);
+    }
+
+    public String getMessage() {
+        return super.getMessage();
+    }
     
 }
