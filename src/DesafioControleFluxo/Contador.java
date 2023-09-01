cd5e8210a37b97d58935ed290cb26c13067a4179
GUSTAVO ANTONIOLI MEDEIROS <94537799+GustaAntonioli@users.noreply.github.com>
2023-09-01 20:15:31 -0300

    -

--- src/DesafioControleFluxo/Contador.java
+++ src/DesafioControleFluxo/Contador.java
@@ -6,35 +6,32 @@ public class Contador {
 
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
-		
-		try {
-			// Lendo o primeiro parâmetro
+			// Primeiro parâmetro
 			System.out.println("Informe o primeiro número: ");
 			int parametroUm = Integer.parseInt(terminal.nextLine());
-			
-			// Lendo o segundo parâmetro
+			// Segundo parâmetro
 			System.out.println("Informe o segundo número: ");
 			int parametroDois = Integer.parseInt(terminal.nextLine());
 			
-			// Validando se parametroDois é maior ou igual que parametroUm
+			// Fecha terminal
+			terminal.close();
+		try {
 			if (parametroDois <= parametroUm){
 				throw new ParametrosInvalidosException("O segundo número deve ser maior ou igual ao primeiro.");
-			}
 			
-			// Imprimindo os números
+			} else 
 			for (int i = parametroUm; i <= parametroDois; i--) {
 				if (i == parametroUm || i == parametroDois) {
 					System.out.println("--Fim--");
+					}
 				}
 			}
 		
-		}catch (ParametrosInvalidosException exception) {
+		catch (ParametrosInvalidosException exception) {
 			//imprimir a mensagem: O segundo parâmetro deve ser maior ou igual ao primeiro
             System.out.println(exception.getMessage());
         }
-		
-		// fechando o scanner
-		terminal.close();
-		
 	}
 }
+
+
