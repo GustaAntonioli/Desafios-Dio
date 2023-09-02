4697cf792e57609ca11fc09f8da44747f880ab53
GUSTAVO ANTONIOLI MEDEIROS <94537799+GustaAntonioli@users.noreply.github.com>
2023-09-02 10:37:34 -0300

    Concluído

--- src/DesafioControleFluxo/Contador.java
+++ src/DesafioControleFluxo/Contador.java
@@ -6,36 +6,41 @@ public class Contador {
 
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
-			// Primeiro parâmetro
-			System.out.println("Informe o primeiro número: ");
-			int parametroUm = Integer.parseInt(terminal.nextLine());
-			// Segundo parâmetro
-			System.out.println("Informe o segundo número: ");
-			int parametroDois = Integer.parseInt(terminal.nextLine());
-			
-			// Fecha terminal
-			terminal.close();
-		try { // Validação de ParametrosInvalidosException
-			if (parametroDois <= parametroUm){
-				throw new ParametrosInvalidosException("O segundo número deve ser maior ou igual ao primeiro.");
-			
-			} else 
+		
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		terminal.close();	
 
-			for (int i = parametroUm; i <= parametroDois;) {
-				if (i == parametroUm || i == parametroDois) {
-					System.out.print("A diferença entre os parâmetros é: ");
+        try {
+			//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+			if (parametroDois <= parametroUm){
+				throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+			}
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println(exception.getMessage());
+		}
+		
+	}
+
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		
+		int contagem = parametroDois - parametroUm;
+		
+		for (int i = 0; i < contagem; i++) {
+			if (i == parametroUm || i == parametroDois) {
+				    System.out.print("A diferença entre os parâmetros é: ");
 					System.out.println(parametroDois - parametroUm);
 					System.out.println("--Fim--");
-					}
-					break;
-				}
+                    //System.out.println(i + 1);
+                    break;
 			}
-		
-		catch (ParametrosInvalidosException exception) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior ou igual ao primeiro
-            System.out.println(exception.getMessage());
-        }
+		}
 	}
 }
-
-
